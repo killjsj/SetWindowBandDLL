b0f87c51ad7eb349d0aec685794318374ef99d10
killjsj <killjsj@outlook.com>
2025-02-08 12:15:04 +0800

    bug fix//exit = FreeLibrary

--- WHY.h
+++ WHY.h
@@ -23,35 +23,3 @@ using namespace std;
 
 #pragma comment(lib, "ws2_32.lib")
 
-void initializeWinsock() {
-    WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "Winsock initialization failed!" << std::endl;
-        exit(1);
-    }
-}
-
-SOCKET createListeningSocket(int port) {
-    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == INVALID_SOCKET) {
-        std::cerr << "Socket creation failed!" << std::endl;
-        exit(1);
-    }
-
-    sockaddr_in serverAddr = {};
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(port);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        std::cerr << "Bind failed!" << std::endl;
-        exit(1);
-    }
-
-    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
-        std::cerr << "Listen failed!" << std::endl;
-        exit(1);
-    }
-
-    return serverSocket;
-}
